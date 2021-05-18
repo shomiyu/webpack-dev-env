0a44f510172d31c79d323a5afaaf7bd562fe9e68
miyuki shoji <0428.miyuki.shoji@gmail.com>
2021-05-19 00:19:53 +0900

    💄スタイル設定周りの修正

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
     'stylelint-config-recommended-scss',
     'stylelint-prettier/recommended',
   ],
@@ -11,12 +10,8 @@ module.exports = {
     'order/properties-alphabetical-order': true,
     'string-quotes': 'single',
     'color-hex-case': 'lower',
-    'color-hex-length': 'short',
-    'font-weight-notation': 'named-where-possible',
-    'font-family-name-quotes': 'always-where-recommended',
     'function-url-quotes': 'always',
     'no-descending-specificity': null,
-    'property-no-vendor-prefix': true,
-    'value-no-vendor-prefix': true,
+    'no-duplicate-selectors': null,
   },
 };
