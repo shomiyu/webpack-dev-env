229a53547e11328eed67758d5eae590fc3c77c09
miyuki shoji <0428.miyuki.shoji@gmail.com>
2021-05-18 10:03:28 +0900

    🎉Pugの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,42 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const globule = require('globule');
+
+const dir = {
+  src: 'src',
+  dist: 'dist',
+};
+
+const template = [];
+const documents = globule.find(`./${dir.src}/pug/**/*.pug`, {
+  ignore: `./${dir.src}/pug/**/_*.pug`,
+});
+
+documents.forEach((document) => {
+  const filename = document
+    .replace(`./${dir.src}/pug/`, '')
+    .replace('.pug', '.html');
+
+  template.push(
+    new HtmlWebpackPlugin({
+      filename: filename,
+      template: document,
+      inject: false,
+      minify: false,
+    }),
+  );
+});
 
 module.exports = {
   mode: 'development',
   entry: {
-    main: ['./src/js/index.js', './src/scss/common.scss'],
+    main: [`./${dir.src}/js/index.js`, `./${dir.src}/scss/common.scss`],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, dir.dist),
     filename: './assets/js/[name].bundle.js',
   },
   devtool: 'source-map',
@@ -35,6 +62,23 @@ module.exports = {
           'import-glob-loader',
         ],
       },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              sources: false,
+            },
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -48,5 +92,6 @@ module.exports = {
       fix: true,
       failOnError: true,
     }),
+    ...template,
   ],
 };
