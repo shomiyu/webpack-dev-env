0b7efd2eadcdcd53bb7fe79d55ccc94e134dcf5c
shomiyu <m1011522@gmail.com>
2023-03-21 18:54:57 +0900

    不要なコード削除

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,3 @@
 import { hello } from './modules/test';
 
 hello();
-
-const init = (settings) => {
-  const defaultOptions = {
-    options1: false,
-    options2: false,
-    options3: false,
-  };
-
-  // 変更のあったものだけ検知してoptionsを変更する
-  const options = Object.assign(defaultOptions, settings);
-  console.log(options);
-};
-
-console.log(init());
